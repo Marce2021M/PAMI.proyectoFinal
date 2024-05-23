028683f9b5c016e6ff23a417d615a2da15de3c7c
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2024-05-23 16:37:29 -0600

    haciendo producto final

--- anlysis.R
+++ anlysis.R
@@ -1,85 +1,52 @@
-
-datosInstrumentos <- read.csv("instrumentos.csv", header = TRUE)
-
-View(datosInstrumentos)
-
-unique(datosInstrumentos$instrumento)
-
-colnames(datosInstrumentos) <- c("Marca.temporal","instrumento", "precio", "dañadoAlgunaVez", "frecDaños", "severidad1", "severidad2", "severidad3", "severidad4" ,"severidad5", "roboAlgunaVez", "arreglo", "tipoDañoFrec", "seguroDisposicion", "pagarDisposicion", "None")
-
-unique(datosInstrumentos$categoria)
-
-library(dplyr)
-datosInstrumentos <- datosInstrumentos %>%
-  mutate(categoria = case_when(
-    grepl("Piano|piano|Teclado", instrumento, ignore.case = TRUE) ~ "Piano",
-    grepl("Guitarra|Bajo eléctrico|Ukulele|Cello|Violín|Viola", instrumento, ignore.case = TRUE) ~ "Cuerda",
-    grepl("batería|Batería", instrumento, ignore.case = TRUE) ~ "Batería",
-    grepl("Flauta transversal|Saxofón", instrumento, ignore.case = TRUE) ~ "Viento",
-    TRUE ~ "Otros"  # Cualquier otro instrumento no especificado anteriormente
-  ))
-
-datosInstrumentos$precio <- factor(datosInstrumentos$precio)
-
-hist(datosInstrumentos$frecDaños)
-library(ggplot2)
-ggplot(datosInstrumentos, aes(x = frecDaños)) +
-  geom_bar(stat = "count", fill = "skyblue", color = "black") +
-  labs(title = "Frecuencia de Daños en Instrumentos",
-       x = "Frecuencia de Daños",
-       y = "Cantidad") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Ajusta el texto del eje x para mejor visualización
-
-ggplot(datosInstrumentos, aes(x = severidad1)) +
-  geom_bar(stat = "count", fill = "skyblue", color = "black") +
-  labs(title = "Severidad de Daños en Instrumentos",
-       x = "Severidad de Daños",
-       y = "Cantidad") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Ajusta el texto del eje x para mejor visualización
-
-library(stringr)
-
-datosInstrumentos <- datosInstrumentos %>%
-  mutate(severidad1 = str_replace_all(severidad1, "[^0-9.]", "")) %>% # Elimina todo excepto números y puntos
-  mutate(severidad1 = ifelse(severidad1 == "", NA, as.numeric(severidad1))) # Convierte a numérico, asigna NA si está vacío
-
-
-unique(datosInstrumentos$severidad5)
-
-# Definitivo
-
-library(dplyr)
-library(stringr)
-
-datosInstrumentos <- datosInstrumentos %>%
-  mutate(across(starts_with("severidad"), ~ str_replace_all(., "[^0-9]", "")), # Eliminar caracteres no numéricos
-         across(starts_with("severidad"), ~ ifelse(. == "", NA, as.numeric(.)))) # Convertir a numérico
-
-
-# Función para categorizar valores
-categorizar_valores <- function(x) {
-  cut(x, breaks = c(-Inf, 0, 1000, 3000, 5000, 10000, Inf),
-       labels = c("0", ">0-1000", "1000-3000", "3000-5000", "5000-10000", ">10000"),
-       include.lowest = TRUE)
+# Ajustar las distribuciones utilizando optimización
+ajustar_distribucion <- function(logverosimilitud, start_params) {
+  fit <- optim(start_params, logverosimilitud, method = "SANN")
+  return(fit)
 }
 
-# Aplicar la función de categorización
-datosInstrumentos <- datosInstrumentos %>%
-  mutate(across(starts_with("severidad"), categorizar_valores, .names = "categoria_{.col}"))
+# Ajustar las distribuciones
+for(i in 1:1000){
+(params_weibull <- optim(fn = logverosimilitud_weibull,
+method = "L-BFGS-B", lower = c(0, 0), upper = c(10, 10), par = c(1000, 1000))
+}
 
-library(purrr)
-# Crear lista con las tablas de frecuencia para cada categoría
-lista_frecuencias <- map(datosInstrumentos %>% select(starts_with("categoria_severidad")),table)
-
-names(lista_frecuencias) <- paste("frecuencia", names(lista_frecuencias), sep = "_")
-
-# Transformar cada tabla en un data frame y preparar para ggplot
-data_frames <- map(lista_frecuencias, ~ as.data.frame(.))
-data_frames <- map2(data_frames, names(data_frames), ~ mutate(.x, Category = .y))
-
-# TABLAS DE FRECUENCIA ------------------------
+(params_gamma <- optim(fn = logverosimilitud_gamma, 
+method = "SANN", lower = c(0, 0), upper = c(10, 10), par = c(1, 1)))
+(params_lognormal <- optim(fn = logverosimilitud_lognormal,
+method = "SANN", lower = c(0, 0), upper = c(10, 10), par = c(1, 1)))
+(params_pareto <- optim(fn = logverosimilitud_pareto,
+method = "SANN", lower = c(0, 0), upper = c(10, 10), par = c(1, 1)))
 
 
+# Inicialización de la lista para guardar resultados
+resultados <- vector("list", 1000)
 
+# Bucle para probar diferentes puntos de inicio
+for(i in 1:100000) {
+  # Generar puntos de inicio aleatorios
+  par_ini <- runif(2, min = 0, max = 1000)
+  
+  # Realizar la optimización
+  # Intentar realizar la optimización y manejar errores
+  optim_result <- try(optim(par = par_ini, fn = logverosimilitud_weibull,
+                            method = "L-BFGS-B", lower = c(0, 0), upper = c(1000, 1000)), silent = TRUE)
+  
+  # Verificar si la optimización fue exitosa
+  if (inherits(optim_result, "try-error")) {
+    # Si hubo un error, continuar con la siguiente iteración
+    next
+  }
+  # Guardar los resultados
+  resultados[[i]] <- list(
+    par_ini = par_ini,
+    par_opt = optim_result$par,
+    convergencia = optim_result$convergence,
+    valor = optim_result$value
+  )
+}
+
+# Convertir la lista de resultados a un data frame para su análisis
+resultados_df <- do.call(rbind, lapply(resultados, as.data.frame))
+
+# Mostrar los resultados
+print(resultados_df)
